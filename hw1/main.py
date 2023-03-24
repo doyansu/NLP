e596ec703254e991b36b65f22fc1f9e0d7498172
doyansu <fyuanhao91@gmail.com>
2023-03-24 14:26:12 +0800

    [hw1] first commit

---
+++ hw1/main.py
@@ -0,0 +1,7 @@
+# import numpy as np
+# import math
+# import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import confusion_matrix
+# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+# from sklearn.linear_model import LinearRegression
